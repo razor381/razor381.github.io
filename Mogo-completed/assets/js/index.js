3c5e1fc184d6fe8b12b6a94ca7c3103bf46d01d9
Rujal Shrestha <rujal4shrz@gmail.com>
2021-05-04 22:57:01 +0545

    update evaluated and validated changes

--- Mogo-completed/assets/js/index.js
+++ Mogo-completed/assets/js/index.js
@@ -1,29 +1,39 @@
 const menuBtn = document.querySelector('#menu-btn');
 const hiddenMenuEl = document.querySelector('#hidden-menu-el');
 const menuCloseBtn = document.querySelector('#menu-close-btn');
+const body = document.querySelector('body');
+
+const POSITION_FIXED = 'fixed';
+const POSITION_INITIAL = 'initial';
+const DISPLAY_NONE = 'none';
+const DISPLAY_BLOCK = 'block';
+const MOBILE_SCREEN_WIDTH = 559;
 
 const getHiddenMenuDisplayStatus = () => hiddenMenuEl.style.display;
 
 function closeHiddenMenu() {
   const screenWidth = document.documentElement.clientWidth;
 
-  if ((screenWidth > 559) && getHiddenMenuDisplayStatus() !== 'none') {
-    hiddenMenuEl.style.display = 'none';
+  if ((screenWidth > MOBILE_SCREEN_WIDTH) && getHiddenMenuDisplayStatus() !== DISPLAY_NONE) {
+    hiddenMenuEl.style.display = DISPLAY_NONE;
+    body.style.position = POSITION_INITIAL;
   };
 }
 
 if (menuBtn && hiddenMenuEl) {
   menuBtn.addEventListener('click', () => {
-    const display = getHiddenMenuDisplayStatus();
-    hiddenMenuEl.style.display = display === 'block' ? 'none' : 'block';
+    hiddenMenuEl.style.display = DISPLAY_BLOCK;
+    body.style.position = POSITION_FIXED;
   })
 }
 
 if (hiddenMenuEl && menuCloseBtn) {
   menuCloseBtn.addEventListener('click', () => {
-    hiddenMenuEl.style.display = 'none';
+    hiddenMenuEl.style.display = DISPLAY_NONE;
+    body.style.position = POSITION_INITIAL;
   })
 }
 
-window.addEventListener("resize", closeHiddenMenu);
+window.addEventListener('resize', closeHiddenMenu);
+
 
